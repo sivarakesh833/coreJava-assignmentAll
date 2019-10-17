a3e52c1ec33e3c5944d2e7489bc2462079c9893f
rakesh <kvkr9440@gmail.com>
2019-10-17 15:41:49 +0530

    Commited from eclipse

---
+++ Assignment6/src/com/cg/main/Listma.java
@@ -0,0 +1,13 @@
+package com.cg.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Listma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List list = new ArrayList();
+
+	}
+}
